49c7bf86b7514caaf579c9a2dff27f40160e1103
sadman dipto <sadman.dipto2015@gmail.com>
2019-01-06 00:45:15 +0600

    web api angular 6 SPA

---
+++ ProductApi/ProductApi/Controllers/OrderController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ProductApi;
+
+namespace ProductApi.Controllers
+{
+    public class OrderController : ApiController
+    {
+        private MarketingDbEntities db = new MarketingDbEntities();
+
+        // GET api/Order
+        public IQueryable<OrderTbl> GetOrderTbls()
+        {
+            return db.OrderTbls;
+        }
+
+        // GET api/Order/5
+        [ResponseType(typeof(OrderTbl))]
+        public IHttpActionResult GetOrderTbl(int id)
+        {
+            OrderTbl ordertbl = db.OrderTbls.Find(id);
+            if (ordertbl == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ordertbl);
+        }
+
+        // PUT api/Order/5
+        public IHttpActionResult PutOrderTbl(int id, OrderTbl ordertbl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != ordertbl.OrderId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(ordertbl).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderTblExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Order
+        [ResponseType(typeof(OrderTbl))]
+        public IHttpActionResult PostOrderTbl(OrderTbl ordertbl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.OrderTbls.Add(ordertbl);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = ordertbl.OrderId }, ordertbl);
+        }
+
+        // DELETE api/Order/5
+        [ResponseType(typeof(OrderTbl))]
+        public IHttpActionResult DeleteOrderTbl(int id)
+        {
+            OrderTbl ordertbl = db.OrderTbls.Find(id);
+            if (ordertbl == null)
+            {
+                return NotFound();
+            }
+
+            db.OrderTbls.Remove(ordertbl);
+            db.SaveChanges();
+
+            return Ok(ordertbl);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OrderTblExists(int id)
+        {
+            return db.OrderTbls.Count(e => e.OrderId == id) > 0;
+        }
+    }
+}
---
+++ ProductApi/ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ProductApi;
+
+namespace ProductApi.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private MarketingDbEntities db = new MarketingDbEntities();
+
+        // GET api/Product
+        public IQueryable<ProductTbl> GetProductTbls()
+        {
+            return db.ProductTbls;
+        }
+
+        // GET api/Product/5
+        [ResponseType(typeof(ProductTbl))]
+        public IHttpActionResult GetProductTbl(int id)
+        {
+            ProductTbl producttbl = db.ProductTbls.Find(id);
+            if (producttbl == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(producttbl);
+        }
+
+        // PUT api/Product/5
+        public IHttpActionResult PutProductTbl(int id, ProductTbl producttbl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != producttbl.ProductId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(producttbl).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductTblExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Product
+        [ResponseType(typeof(ProductTbl))]
+        public IHttpActionResult PostProductTbl(ProductTbl producttbl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ProductTbls.Add(producttbl);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = producttbl.ProductId }, producttbl);
+        }
+
+        // DELETE api/Product/5
+        [ResponseType(typeof(ProductTbl))]
+        public IHttpActionResult DeleteProductTbl(int id)
+        {
+            ProductTbl producttbl = db.ProductTbls.Find(id);
+            if (producttbl == null)
+            {
+                return NotFound();
+            }
+
+            db.ProductTbls.Remove(producttbl);
+            db.SaveChanges();
+
+            return Ok(producttbl);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductTblExists(int id)
+        {
+            return db.ProductTbls.Count(e => e.ProductId == id) > 0;
+        }
+    }
+}
